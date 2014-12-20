420453ed0bb5a18688915c03cb74d52301a4530f
Henrik Steen <henrist@henrist.net>
2014-12-20 02:07:40 +0100

    Oppdaterte modeller og la inn modeller på admin sia

--- cyb_varer/varer/admin.py
+++ cyb_varer/varer/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from cyb_varer.varer.models import *
+
+class RåvareprisInline(admin.TabularInline):
+    model = Råvarepris
+    verbose_name_plural = 'Priser'
+
+class RåvareAdmin(admin.ModelAdmin):
+    inlines = [RåvareprisInline]
+    search_fields = ['kategori', 'navn', 'innkjøpskonto']
+
+class SalgsvareRåvareInline(admin.TabularInline):
+    model = Salgsvare.råvarer.through
+    extra = 1
+    min_num = 1
+
+class SalgsvareAdmin(admin.ModelAdmin):
+    inlines = [SalgsvareRåvareInline]
+    search_fields = ['kategori', 'navn', 'salgskonto']
+
+class SalgskalkyleVareInline(admin.TabularInline):
+    model = Salgskalkyle.varer.through
+
+class SalgskalkyleAdmin(admin.ModelAdmin):
+    inlines = [SalgskalkyleVareInline]
+    search_fields = ['navn']
+
+class VaretellingVareInline(admin.TabularInline):
+    model = Varetelling.varer.through
+
+class VaretellingAdmin(admin.ModelAdmin):
+    inlines = [VaretellingVareInline]
+    search_fields = ['tittel', 'ansvarlig']
+
+admin.site.register(Råvare, RåvareAdmin)
+admin.site.register(Leverandør)
+#admin.site.register(Råvarepris)
+admin.site.register(Salgsvare, SalgsvareAdmin)
+#admin.site.register(SalgsvareRåvare)
+#admin.site.register(SalgsvarePris)
+admin.site.register(Salgskalkyle, SalgskalkyleAdmin)
+#admin.site.register(SalgskalkyleVare)
+admin.site.register(Varetelling, VaretellingAdmin)
+#admin.site.register(VaretellingVare)
--- cyb_varer/varer/models.py
+++ cyb_varer/varer/models.py
@@ -14,9 +14,23 @@ class Råvare(models.Model):
     innkjøpskonto = models.PositiveSmallIntegerField()
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='OK')
 
+    class Meta:
+        ordering = ['kategori', 'navn']
+        verbose_name_plural = 'råvarer'
+
+    def __str__(self):
+        return (self.kategori + ': ' if self.kategori else '') + self.navn
+
 class Leverandør(models.Model):
     navn = models.CharField(max_length=100)
     kommentar = models.TextField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['navn']
+        verbose_name_plural = 'leverandører'
+
+    def __str__(self):
+        return self.navn
 
 class Råvarepris(models.Model):
     råvare = models.ForeignKey(Råvare, related_name='priser')
@@ -25,11 +39,26 @@ class Råvarepris(models.Model):
     pris = models.FloatField(help_text="Pris eks mva")
     dato = models.DateField()
 
+    class Meta:
+        ordering = ['-dato']
+        verbose_name_plural = 'råvarepriser'
+
+    def __str__(self):
+        return self.råvare.navn + ' (' + self.dato.isoformat() + '): kr ' + str(self.pris)
+
 class Salgsvare(models.Model):
     kategori = models.CharField(max_length=50, null=True, blank=True)
     navn = models.CharField(max_length=100)
     salgskonto = models.PositiveSmallIntegerField()
     status = models.CharField(max_length=10, choices=Råvare.STATUS_CHOICES, default='OK')
+    råvarer = models.ManyToManyField(Råvare, through='varer.SalgsvareRåvare', related_name='salgsvarer')
+
+    class Meta:
+        ordering = ['kategori', 'navn']
+        verbose_name_plural = 'salgsvarer'
+
+    def __str__(self):
+        return (self.kategori + ': ' if self.kategori else '') + self.navn
 
 class SalgsvareRåvare(models.Model):
     salgsvare = models.ForeignKey(Salgsvare)
@@ -51,26 +80,53 @@ class SalgsvarePris(models.Model):
     pris_intern = models.PositiveSmallIntegerField(help_text="Internpris INK mva", null=True, blank=True)
     pris_ekstern = models.PositiveSmallIntegerField(help_text="Eksternpris INK mva", null=True, blank=True)
 
+    class Meta:
+        ordering = ['-dato']
+        verbose_name_plural = 'salgsvarepriser'
+
 class Salgskalkyle(models.Model):
     navn = models.CharField(max_length=30)
     kommentar = models.TextField(null=True, blank=True)
     dato = models.DateField()
+    varer = models.ManyToManyField(Salgsvare, through='varer.SalgskalkyleVare', related_name='salgskalkyler')
+
+    class Meta:
+        ordering = ['-dato']
+        verbose_name_plural = 'salgskalkyler'
+
+    def __str__(self):
+        return '%s (%s)' % (self.navn, self.dato.isoformat())
 
 class SalgskalkyleVare(models.Model):
-    kalkyle = models.ForeignKey(Salgskalkyle, related_name='varer')
-    salgsvare = models.ForeignKey(Salgsvare, related_name='kalkyler')
-    interngrad = models.FloatField(null=True, blank=True)
+    kalkyle = models.ForeignKey(Salgskalkyle)
+    salgsvare = models.ForeignKey(Salgsvare)
+    interngrad = models.FloatField(null=True, blank=True, help_text="Prosent andel (antall enheter) solgt til internpris")
     antall = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['salgsvare']
+        verbose_name_plural = 'salgskalkylevarer'
 
 class Varetelling(models.Model):
     tittel = models.CharField(max_length=50)
     kommentar = models.TextField(null=True, blank=True)
     tid = models.DateTimeField()
     ansvarlig = models.CharField(max_length=100)
+    varer = models.ManyToManyField(Råvare, through='varer.VaretellingVare', related_name='varetellinger')
+
+    class Meta:
+        ordering = ['-tid']
+        verbose_name_plural = 'varetellinger'
+
+    def __str__(self):
+        return '%s (%s)' % (self.tittel, str(self.tid.isoformat()))
 
 class VaretellingVare(models.Model):
-    varetelling = models.ForeignKey(Varetelling, related_name='varer')
+    varetelling = models.ForeignKey(Varetelling)
     råvare = models.ForeignKey(Råvare)
     sted = models.CharField(max_length=50)
     antall = models.FloatField()
     kommentar = models.CharField(max_length=150, null=True, blank=True)
+
+    class Meta:
+        ordering = ['varetelling', 'råvare']
