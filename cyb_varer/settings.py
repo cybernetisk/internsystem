320316d784ca79cc09287bb5c9a225a98ce62cdf
Henrik Steen <henrist@henrist.net>
2014-12-20 18:25:10 +0100

    Språk og tidssone

--- cyb_varer/settings.py
+++ cyb_varer/settings.py
@@ -73,9 +73,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'nb'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Oslo'
 
 USE_I18N = True
 
