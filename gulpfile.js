2b47c96918fa6ce9d2224c69a719eb8c43a94b42
Henrik Steen <henrist@henrist.net>
2014-12-22 22:59:09 +0100

    Angular resource dependency

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,8 @@ var js_files_library = [
     'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
     'bower_components/angular/angular.js',
     'bower_components/angular-ui-router/release/angular-ui-router.min.js',
-    'bower_components/angular-animate/angular-animate.js'
+    'bower_components/angular-animate/angular-animate.js',
+    'bower_components/angular-resource/angular-resource.js'
 ];
 
 var js_files = [
