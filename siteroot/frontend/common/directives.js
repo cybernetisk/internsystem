b808bc64aaacec61b02d8a405c4356531e1e4fc5
Henrik Steen <henrist@henrist.net>
2014-12-22 02:36:00 +0100

    Sett .active på menyelementer

---
+++ siteroot/frontend/common/directives.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+
+    var module = angular.module('cyb.oko');
+
+    /**
+     * Set .active on menu elements when state change
+     */
+    module.directive('navbarCheckActive', function ($state) {
+        return {
+            restrict: 'A',
+            link: function (scope, element) {
+                scope.$on('$stateChangeSuccess', function () {
+                    element.find('.active').removeClass();
+                });
+                element.find('a[ui-sref]').each(function () {
+                    var state = $(this).attr('ui-sref');
+                    var elms = $(this).closest('li').add($(this).parents('li.dropdown'));
+                    scope.$on('$stateChangeSuccess', function () {
+                        if ($state.includes(state)) {
+                            elms.addClass('active');
+                        }
+                    });
+                });
+            }
+        };
+    });
+})();
