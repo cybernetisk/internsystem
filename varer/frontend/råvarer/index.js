f3164d91729d0efa558b1d3ccee2c8e2012a61fe
Henrik Steen <henrist@henrist.net>
2014-12-23 02:17:11 +0100

    Oppdatert frontend for råvarer

--- varer/frontend/råvarer/index.js
+++ varer/frontend/råvarer/index.js
@@ -5,36 +5,29 @@
 
     module.config(function ($stateProvider) {
         $stateProvider.state('råvarer', {
-            url: '/varer/råvarer',
+            url: '/varer/råvarer?page',
             templateUrl: 'views/varer/råvarer/index.html',
-            controller: 'RåvarerController as raavarer'
+            controller: 'RåvarerController as raavarer',
+            reloadOnSearch: false
         })
     });
 
-    module.controller('RåvarerController', function ($scope, RåvarerService) {
+    module.controller('RåvarerController', function ($location, $scope, $stateParams, ParamsHelper, RåvarerService) {
         var self = this;
-        console.log("RåvarerController");
 
-        RåvarerService.query(function(res) {
-            self.items = res.results.map(function(item) {
-                // finn aktiv innkjøpspris
-                item.innpris = null;
-                item.priser.forEach(function (pris) {
-                    if (!item.innpris || pris.dato >= item.innpris.dato)
-                        item.innpris = pris;
+        var helper = ParamsHelper.track($scope,
+            ['page'],
+            {'raavarer.pagination.page': 'page'},
+            function (params) {
+                self.items = null;
+
+                RåvarerService.getList(params).then(function (res) {
+                    self.items = res.results;
+                    self.pagination = res.pagination;
                 });
+            }
+        );
 
-                // finn aktiv salgspris
-                item.salgspris = null;
-                if (item.lenket_salgsvare) {
-                    item.lenket_salgsvare.priser.forEach(function (pris) {
-                        if (!item.salgspris || pris.dato >= item.salgspris.dato)
-                            item.salgspris = pris;
-                    });
-                }
-
-                return item;
-            });
-        });
+        helper.run();
     });
 })();
---
+++ varer/frontend/råvarer/råvarer.service.js
@@ -0,0 +1,46 @@
+(function() {
+    'use strict';
+
+    var module = angular.module('cyb.varer');
+
+    module.factory('RåvarerService', function ($resource) {
+        var obj = $resource('api/råvarer/:id/', {
+            id: '@id'
+        }, {
+            query: {
+                isArray: false,
+                params: {
+                    limit: 30
+                }
+            }
+        });
+
+        obj.getList = function (params) {
+            return this.query(params).$promise.then(function (res) {
+                res.result = res.results.map(function (item) {
+                    // finn aktiv innkjøpspris
+                    item.innpris = null;
+                    item.priser.forEach(function (pris) {
+                        if (!item.innpris || pris.dato >= item.innpris.dato)
+                            item.innpris = pris;
+                    });
+
+                    // finn aktiv salgspris
+                    item.salgspris = null;
+                    if (item.lenket_salgsvare) {
+                        item.lenket_salgsvare.priser.forEach(function (pris) {
+                            if (!item.salgspris || pris.dato >= item.salgspris.dato)
+                                item.salgspris = pris;
+                        });
+                    }
+
+                    return item;
+                });
+
+                return res;
+            });
+        };
+
+        return obj;
+    });
+})();
