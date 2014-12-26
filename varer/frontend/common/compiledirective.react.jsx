c90bfdc4c7e187199d0a9bbad0e077ba97394819
Henrik Steen <henrist@henrist.net>
2014-12-26 22:58:41 +0100

    Wrapperelement for React for å sjekke for directives

---
+++ varer/frontend/common/compiledirective.react.jsx
@@ -0,0 +1,24 @@
+/** @jsx React.DOM */
+/**
+ * Check for directives in a React-element
+ *
+ * Usage:
+ * <CompileDirective elm={<a ui-sref="demo">text</a>}></CompileDirective>
+ *
+ * NB: Currently bound to rootScope
+ */
+angular.module('cyb.varer').factory('CompileDirective', function ($compile, $rootScope) {
+    return React.createClass({
+        render: function () {
+            return this.props.elm;
+        },
+
+        componentDidMount: function () {
+            $compile(this.getDOMNode())($rootScope);
+        },
+
+        componentDidUpdate: function () {
+            $compile(this.getDOMNode())($rootScope);
+        }
+    });
+});
