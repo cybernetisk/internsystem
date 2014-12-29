6659722f4f3598efb12e48fc7778fcc55337d762
Henrik Steen <henrist@henrist.net>
2014-12-29 14:04:43 +0100

    CSRF-token

--- siteroot/frontend/app.js
+++ siteroot/frontend/app.js
@@ -7,12 +7,15 @@
         'cyb.z'
     ]);
 
-    module.config(function ($locationProvider, $urlRouterProvider) {
+    module.config(function ($locationProvider, $urlRouterProvider, $httpProvider) {
         $locationProvider.html5Mode(true);
 
         $urlRouterProvider
             .otherwise(function() {
                 console.log("unknown route");
             });
+
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
     })
 })();
