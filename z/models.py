2c03ecb823d5863c9dc2dd423bc36d6f08b089d3
Sigurd Ljødal <sigurd@ljodal.no>
2015-02-17 22:18:56 +0100

    Forslag til modeller for z-rapport

--- z/models.py
+++ z/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from varer.models import Salgsvare
 
-# Create your models here.
+# Z-rapport
+class Zrapport(models.Model):
+    nummer = models.PositiveIntegerField('Zrapport-nummer', null=False)
+    tidspunkt = models.DateTimeField('Tidspunkt', null=False)
+
+# En kvittering er en kobling mellom en z-rapport og en
+# eller flere transaksjoner i kassen.
+class Kvittering(models.Model):
+    nummer = models.PositiveIntegerField('Kvitteringsnummer', null=False)
+    tidspunkt = models.DateTimeField('Tidspunkt', null=False)
+    varetransaksjoner = models.ForeignKey('Kassetransaksjon', null=False, blank=False)
+
+
+# En varetransaksjon er et uttak av et antall av en salgsvare.
+class Varetransaksjon(models.Model):
+    salgsvare = models.ForeignKey(Salgsvare, null=False, blank=False)
+    antall = models.IntegerField('Antall varer', null=False)
+
+# En kassetransaksjon er en varetransaksjon med en kobling
+# mot en kvittering og derfra videre til en z-rapport.
+class Kassetransaksjon(Varetransaksjon):
+    kvittering = models.ForeignKey(Kvittering, null=False, blank=False)
