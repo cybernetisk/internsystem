4123831b7ca36db47399a6296adef58c53dcd800
Henrik Steen <henrist@henrist.net>
2015-03-31 19:04:37 +0200

    Update deprecated method name

--- varer/managers.py
+++ varer/managers.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class RåvareManager(models.Manager):
-    def get_query_set(self):
+    def get_queryset(self):
         return super(RåvareManager, self).get_query_set().select_related('innkjopskonto')
