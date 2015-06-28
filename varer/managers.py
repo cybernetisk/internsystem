6ca1512258c453ef8c80787f69d5cd5bcbf31ef7
Henrik Steen <henrist@henrist.net>
2015-06-28 04:37:55 +0200

    Fix usage of get_query_set to be get_queryset

--- varer/managers.py
+++ varer/managers.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class RåvareManager(models.Manager):
     def get_queryset(self):
-        return super(RåvareManager, self).get_query_set().select_related('innkjopskonto')
+        return super(RåvareManager, self).get_queryset().select_related('innkjopskonto')
