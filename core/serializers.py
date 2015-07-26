e980b2950cbf31faea66064cea1691901329b1db
Henrik Steen <henrist@henrist.net>
2015-07-26 02:41:29 +0200

    Serializers for representing user object

--- core/serializers.py
+++ core/serializers.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from core.models import User
+
+class UserSimpleGuestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'realname')
+
+
+class UserSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'realname', 'email')
+
+
+class UserExtendedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'realname', 'email', 'is_superuser', 'is_staff', 'is_active', 'date_joined',
+                  'groups', 'user_permissions')
