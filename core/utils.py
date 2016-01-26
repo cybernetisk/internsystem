0bdc19cea29701ac765eb9241ef41793afd5f737
Henrik Steen <henrist@henrist.net>
2016-01-27 00:35:38 +0100

    Method for getting semester of a specific date

--- core/utils.py
+++ core/utils.py
@@ -3,11 +3,12 @@ from datetime import datetime
 from rest_framework.routers import SimpleRouter, DefaultRouter
 import math
 
-"""Get the Semester-object relative to this semester. Create it if needed."""
+
 def get_semester(semester_offset=0):
+    """Get the Semester-object relative to this semester. Create it if needed."""
     now = datetime.now()
     year = now.year
-    sem = int(math.ceil(now.month/6.0))  # returns 1 or 2
+    sem = int(math.ceil(now.month / 6.0))  # returns 1 or 2
 
     # calculate correct year and sem from offset
     if semester_offset != 0:
@@ -23,16 +24,22 @@ def get_semester(semester_offset=0):
     return obj
 
 
-"""Get year and semester details, but not a real semester object."""
 def get_semester_details_from_date(date):
+    """Get year and semester details, but not a real semester object."""
     return {
         'year': date.year,
         'semester': Semester.FALL if date.month > 6 else Semester.SPRING
     }
 
 
-"""Shared API-router to maintain a shared DefaultRouter for application urls"""
+def get_semester_of_date(date):
+    """Get the Semester-object of a specific date. Create it if needed."""
+    details = get_semester_details_from_date(date)
+    return Semester.objects.get_or_create(year=details['year'], semester=details['semester'])[0]
+
+
 class SharedAPIRootRouter(SimpleRouter):
+    """Shared API-router to maintain a shared DefaultRouter for application urls"""
     shared_router = DefaultRouter(trailing_slash=False)
 
     def register(self, *args, **kwargs):
