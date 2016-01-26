690cf895bc4dd5cff307473e2079ce3267521016
Henrik Steen <henrist@henrist.net>
2016-01-26 17:21:54 +0100

    Add missing migration

---
+++ voucher/migrations/0005_auto_20160126_1721.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('voucher', '0004_auto_20160124_2143'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='voucheruselog',
+            options={'ordering': ['-date_spent']},
+        ),
+        migrations.AlterModelOptions(
+            name='worklog',
+            options={'ordering': ['-date_issued']},
+        ),
+        migrations.AlterField(
+            model_name='voucheruselog',
+            name='wallet',
+            field=models.ForeignKey(related_name='uselogs', to='voucher.VoucherWallet'),
+        ),
+        migrations.AlterField(
+            model_name='worklog',
+            name='wallet',
+            field=models.ForeignKey(related_name='worklogs', to='voucher.VoucherWallet'),
+        ),
+    ]
