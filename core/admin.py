a179c124e9d049665c52282bdaed925d1908bbe9
Henrik Steen <henrist@henrist.net>
2016-01-28 21:24:56 +0100

    Change add user form for admin page to not ask for password

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,22 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as _UserAdmin
 from core.models import User, Semester, Card
 
 from django.utils.translation import ugettext_lazy as _
+
+
+class UserCreationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        if commit:
+            user.save()
+        return user
+
 
 class UserAdmin(_UserAdmin):
     fieldsets = (
@@ -12,10 +26,20 @@ class UserAdmin(_UserAdmin):
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username',),
+        }),
+    )
+
     list_display = ('username', 'email', 'realname', 'is_staff')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
 
     search_fields = ('username', 'realname', 'email')
+
+    add_form = UserCreationForm
+    add_form_template = None
 
 
 admin.site.register(User, UserAdmin)
