00b22be1cf53cb7666ba316b80c3aa48c275a66d
Mats Schjølberg <mats@schjolberg.net>
2016-01-30 01:10:33 +0100

    Got member add and member list endpoints up and running

--- members/rest.py
+++ members/rest.py
@@ -1,16 +1,49 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from members.serializers import *
+from core.models import User
+from core.utils import get_semester_of_date
+import datetime
 
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
-    serializer_class = MemberSerializer
+    permission_classes = (IsAuthenticated,)
 
-    def create(self, request, username=None):
-        adder = self.get_object()
-        return Response(self.serializer_class)
+    def get_serializer_class(self):
+        if self.action in ['create']:
+            return AddMemberSerializer
+        return MemberSerializer
+
+    def create(self, request, **kwargs):
+        serializer = AddMemberSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        id = request.user
+        adder = User.objects.get(username=id)
+        date_joined = datetime.datetime.now()
+        semester = get_semester_of_date(date_joined)
+        try:
+            user = User.objects.get(email=serializer.data['email'])
+        except:
+            user = None
+        member = Member(
+            seller=adder,
+            date_joined=date_joined,
+            semester=semester,
+            name=serializer.data['name'],
+            lifetime=serializer.data['lifetime'],
+            email=serializer.data['email'],
+            honorary=False
+        )
+        if user is not None:
+            member.user = user
+        member.save()
+
+        return Response(member.id)
+
+
 
 
     def list(self, request, username=None):
--- members/serializers.py
+++ members/serializers.py
@@ -7,3 +7,14 @@ class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
         fields = ('id', 'name', 'email', 'semester', 'lifetime', 'honorary')
+
+
+class AddMemberSerializer(serializers.Serializer):
+    '''
+    class Meta:
+        model = Member
+        field = ('id', 'name', 'email', 'lifetime', 'honorary', 'user')
+    '''
+    name = serializers.CharField(max_length=50, help_text='name of user, max 50 letters')
+    email = serializers.EmailField(allow_blank=True)
+    lifetime = serializers.BooleanField(help_text='Is the member a lifetime member?')
