e87e1d78c8180f675ad88137316093eb04ba6f2f
Henrik Steen <henrist@henrist.net>
2016-02-09 20:53:45 +0100

    Add filtering of dates for vouchers

--- voucher/filters.py
+++ voucher/filters.py
@@ -1,17 +1,40 @@
 import django_filters
+from datetime import datetime, timedelta
+from rest_framework.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from django.utils.timezone import get_current_timezone
 
 from core.models import Card
 from voucher.models import UseLog, Wallet, WorkLog
 from voucher.utils import get_valid_semesters
 
 
+def apply_date_filter(queryset, value, field, lte):
+    try:
+        t = datetime.strptime(value, '%Y-%m-%d').replace(tzinfo=get_current_timezone())
+        field += '__lte' if lte else '__gte'
+        if lte:
+            t += timedelta(days=1)
+        return queryset.filter(**{field: t})
+    except ValueError:
+        raise ValidationError(detail=_('Date format of %(date)s not recognized') % {'date': value})
+
+
 class UseLogFilter(django_filters.FilterSet):
     user = django_filters.CharFilter(name='wallet__user__username')
     semester = django_filters.CharFilter(name='wallet__semester')
+    date_from = django_filters.MethodFilter(action='filter_date_from')
+    date_to = django_filters.MethodFilter(action='filter_date_to')
 
     class Meta:
         model = UseLog
         fields = ['id', 'user', 'semester']
+
+    def filter_date_from(self, queryset, value):
+        return apply_date_filter(queryset, value, 'date_spent', lte=False)
+
+    def filter_date_to(self, queryset, value):
+        return apply_date_filter(queryset, value, 'date_spent', lte=True)
 
 
 class WalletFilter(django_filters.FilterSet):
@@ -37,7 +60,15 @@ class WorkLogFilter(django_filters.FilterSet):
     user = django_filters.CharFilter(name='wallet__user__username')
     issuing_user = django_filters.CharFilter(name='issuing_user__username')
     semester = django_filters.CharFilter(name='wallet__semester')
+    date_from = django_filters.MethodFilter(action='filter_date_from')
+    date_to = django_filters.MethodFilter(action='filter_date_to')
 
     class Meta:
         model = WorkLog
         fields = ['id', 'user', 'issuing_user', 'semester']
+
+    def filter_date_from(self, queryset, value):
+        return apply_date_filter(queryset, value, 'date_worked', lte=False)
+
+    def filter_date_to(self, queryset, value):
+        return apply_date_filter(queryset, value, 'date_worked', lte=True)
