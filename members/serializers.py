ca5327e963ced5c6ed0a92c438db906ab38fdd7e
Mats Schjølberg <mats@schjolberg.net>
2016-02-23 16:46:41 +0100

    Edits in serializers for getting seller information in api calls.

--- members/serializers.py
+++ members/serializers.py
@@ -2,16 +2,19 @@ from rest_framework import serializers
 
 from members.models import Member
 from core.serializers import SemesterSerializer
+from core.serializers import UserSimpleSerializer
 
 
 class MemberSerializer(serializers.ModelSerializer):
     semester = SemesterSerializer(read_only=True)
+    seller = UserSimpleSerializer(read_only=True)
 
 
     class Meta:
         model = Member
         fields = (
-        'id', 'name', 'email', 'date_joined', 'semester', 'lifetime', 'honorary', 'date_lifetime', 'uio_username')
+            'id', 'name', 'email', 'date_joined', 'semester', 'lifetime', 'honorary', 'date_lifetime', 'uio_username',
+            'seller')
 
 
 class AddMemberSerializer(serializers.Serializer):
