e3694c87427bf662fc73a70fd8796c69b404f15a
Mats Schjølberg <mats@schjolberg.net>
2016-04-13 13:56:25 +0200

    Added more information in admin interface for groups, intern and roles.

--- intern/admin.py
+++ intern/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from intern.models import Intern, InternGroup, InternRole, AccessLevel
 
+
+class InternAdmin(admin.ModelAdmin):
+    list_display = ('user', 'active')
+    list_filter = ['semester']
+
+
+class InternRoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+class InternGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'leader', 'description')
+
 # Register your models here.
-admin.site.register(Intern)
-admin.site.register(InternRole)
-admin.site.register(InternGroup)
+admin.site.register(Intern, InternAdmin)
+admin.site.register(InternRole, InternRoleAdmin)
+admin.site.register(InternGroup, InternGroupAdmin)
 admin.site.register(AccessLevel)
