e2c3f0a3281ebe23fcf3986eed71eda869df1482
Mats Schjølberg <mats@schjolberg.net>
2016-04-13 12:17:51 +0200

    Added filters to admin interface for members.

--- members/admin.py
+++ members/admin.py
@@ -6,6 +6,7 @@ from members.models import Member
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('name', 'date_joined', 'lifetime', 'honorary', 'seller')
     search_fields = ['name']
+    list_filter = ('lifetime', 'honorary', 'seller')
 
 
 # Register your models here.
