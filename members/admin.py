2270d46967b0650efb85f63f35cdd766b9f6a736
Mats Schjølberg <mats@schjolberg.net>
2016-04-13 11:47:01 +0200

    Added more information to the admin interface for members.

--- members/admin.py
+++ members/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from members.models import Member
 
 
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_joined', 'lifetime', 'honorary', 'seller')
+
+
 # Register your models here.
-admin.site.register(Member)
+admin.site.register(Member, MemberAdmin)
