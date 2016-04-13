0181a23cc64c2448341dfe6720e0a2dc819d60c2
Mats Schjølberg <mats@schjolberg.net>
2016-04-13 12:25:25 +0200

    Removed seller filter, added semester instead.

--- members/admin.py
+++ members/admin.py
@@ -6,7 +6,7 @@ from members.models import Member
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('name', 'date_joined', 'lifetime', 'honorary', 'seller')
     search_fields = ['name']
-    list_filter = ('lifetime', 'honorary', 'seller')
+    list_filter = ('semester', 'lifetime', 'honorary')
 
 
 # Register your models here.
