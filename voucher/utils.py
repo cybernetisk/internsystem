769fa006e2e434104ac47a25d5017da5a96ae4d3
Nicolas "SliZe" Eide <nicolaei@student.matnat.uio.no>
2016-07-07 19:49:10 +0200

    Fixed that you couldn't use last semster's vouchers in  July

--- voucher/utils.py
+++ voucher/utils.py
@@ -6,7 +6,7 @@ from core.utils import get_semester
 def get_valid_semesters():
     semesters = []
     now = datetime.datetime.now()
-    if now.month == 8 or now.month == 1:
+    if now.month == 7 or now.month == 8 or now.month == 1:
         semesters.append(get_semester(-1))
     semesters.append(get_semester())
     return semesters
