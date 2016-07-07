4cca4946cf140033d5c04be5a4a627bcbf873dec
Nicolas "SliZe" Eide <nicolaei@student.matnat.uio.no>
2016-07-07 19:19:37 +0200

    Apparently the NFC card url was gone for some reason

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url, patterns
 from core.views import me
 
-from core.rest import CardViewSet, UserViewSet
+from core.rest import CardViewSet, UserViewSet, NfcCardViewSet
 from core.utils import SharedAPIRootRouter
 
 # SharedAPIRootRouter is automatically imported in global urls config
 router = SharedAPIRootRouter()
 router.register(r'core/users', UserViewSet, base_name='users')
 router.register(r'core/cards', CardViewSet, base_name='voucher_cards')
+router.register(r'core/nfc', NfcCardViewSet)
 
 urlpatterns = patterns('',
     url(r'^api/me$', me, name='me'),
