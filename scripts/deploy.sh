bc5cac66f1d79dcdd42a717d271bac4164f4cbae
Henrik Steen <henrist@henrist.net>
2016-07-21 16:01:51 +0200

    Update deploying with environment, also deploy test branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,11 +14,21 @@ if [ ! -z "$TRAVIS" ]; then
   ssh-add travis-key
 fi
 
+env=''
+if [ "$TRAVIS_BRANCH" == "test" ]; then
+    env=test
+elif [ "$TRAVIS_BRANCH" == "master" ]; then
+    env=prod
+else
+    >&2 echo "Unkown branch '$TRAVIS_BRANCH'"
+    exit 1
+fi
+
 echo "Running remote SSH-script"
 ssh -o StrictHostKeyChecking=no root@in.cyb.no /bin/bash << EOF
   set -e
-  cd ~/drift/internsystem
-  ./update.sh
+  cd ~/drift/internsystem-backend
+  ENV=$env ./update.sh
 EOF
 
 echo "Deploy finished"
