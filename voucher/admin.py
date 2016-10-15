faae190a3100d43153caa52ccdf380b5cfedb472
Mats Astrup Schjølberg <mats@schjolberg.net>
2016-10-15 18:49:54 +0200

    Added some filtering posibilities in admin panel.

--- voucher/admin.py
+++ voucher/admin.py
@@ -7,10 +7,14 @@ class WalletAdmin(admin.ModelAdmin):
     model = Wallet
     readonly_fields = ('cached_balance',)
 
+class WorkLogAdmin(admin.ModelAdmin):
+    model = WorkLog
+    list_filter = ('wallet', 'work_group')
+
 
 admin.site.register(VoucherWallet, WalletAdmin)
 admin.site.register(CoffeeWallet, WalletAdmin)
 admin.site.register(CoffeeRegisterLog)
-admin.site.register(WorkLog)
+admin.site.register(WorkLog, WorkLogAdmin)
 admin.site.register(VoucherUseLog)
 admin.site.register(CoffeeUseLog)
