0574db7a8f164924b6c6f6b5703950b3f628fc4d
Maximiliano Horta <maximilianohorta94@gmail.com>
2018-09-11 22:29:15 +0200

    atomic=False for sql fuckings fuck

--- voucher/migrations/0010_rename_voucher.py
+++ voucher/migrations/0010_rename_voucher.py
@@ -6,6 +6,7 @@ from django.conf import settings
 
 
 class Migration(migrations.Migration):
+    atomic = False
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
