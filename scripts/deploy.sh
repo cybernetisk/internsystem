ed9f52969f6eec4615ae275b14e69a3003188814
Henrik Steen <henrist@henrist.net>
2018-10-28 00:43:22 +0200

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,11 @@ set -eu
 
 if [ ! -z "$TRAVIS" ]; then
   echo "Decrypting ssh-key and adding"
-  openssl aes-256-cbc -K $encrypted_e3b76757b809_key -iv $encrypted_e3b76757b809_iv -in travis-key.enc -out travis-key -d
+  openssl aes-256-cbc -K $encrypted_e3b76757b809_key -iv $encrypted_e3b76757b809_iv -in scripts/travis-key.enc -out travis-key -d
   chmod 600 travis-key
   eval "$(ssh-agent)"
   ssh-add travis-key
+  rm travis-key
 fi
 
 env=''
