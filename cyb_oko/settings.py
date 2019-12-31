019bf825f77cb1857bd80b255a9a4047a63fc3f0
Henrik Steen <henrist@henrist.net>
2019-12-31 01:44:34 +0100

    Remove SameSite for session cookie

--- cyb_oko/settings.py
+++ cyb_oko/settings.py
@@ -86,6 +86,11 @@ CORS_ORIGIN_WHITELIST = (
 )
 CORS_ALLOW_CREDENTIALS = True
 
+# Disable SameSite for session cookie as we need this
+# for auth to work when using local development server
+# of frontend.
+SESSION_COOKIE_SAMESITE = None
+
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
