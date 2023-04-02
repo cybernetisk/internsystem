e342fe0a27a3d503bce77065415e44c0d65992dd
Henrik Steen <henrist@henrist.net>
2023-04-02 09:32:18 +0200

    Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -eu
 env=''
 if [ "$GITHUB_REF_NAME" == "test" ]; then
     env=test
-elif [ "$GITHUB_REF_NAME" == "master" ]; then
+elif [ "$GITHUB_REF_NAME" == "hst" ]; then
     env=prod
 else
     >&2 echo "Unkown branch '$GITHUB_REF_NAME'"
@@ -15,7 +15,7 @@ repo=$(cat .dockerrepo)
 tag=$(cat .dockertag)
 
 echo "Running remote SSH-script"
-ssh root@in.cyb.no /bin/bash << EOF
+ssh -v root@in.cyb.no /bin/bash << EOF
   set -e
   cd ~/drift/internsystem-backend
   ENV=$env ./deploy.sh $tag
